0254c1960d7fcd94c10791d60fd990b556dec3a9
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-17 12:21:54 +0530

    hello world started

---
+++ 04.variable.sh
@@ -0,0 +1,7 @@
+Person1=$1
+Person2=$2
+
+echo "Hi $Person1 how are you"
+echo "I am good, how about you $Person2"
+echo "I am good $Person2, when are you coming to hyderabad"
+echo "I will be there by next week $Person1. We will meet next week $Person2"
