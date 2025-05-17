8c0602ce40333b17c363cc4d927aa97f28bb6e9d
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-17 11:59:37 +0530

    hello world started

---
+++ 02.coversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Hi lokesh how are you"
+echo "I am good, how about you rakesh"
+echo "I am good rakesh, when are you coming to hyderabad"
+echo "I will be there by next week lokesh. We will meet next week rakesh"
