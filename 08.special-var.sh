cd34e4cc11f6b5503ddef73c677397b2a71331a3
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-19 10:59:01 +0530

    hello world started

--- 08.special-var.sh
+++ 08.special-var.sh
@@ -7,3 +7,5 @@ echo "current directory: $pwd"
 echo "user running this script: $USER"
 echo "Home directory of the user: $HOME"
 echo "PID of the script: $$"
+sleep 10 &
+echo "PID of the last command in background: $!"
