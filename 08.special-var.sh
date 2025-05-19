9b70d51ae6acf9b634cb97b49b0efe04e2583058
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-19 10:57:51 +0530

    hello world started

--- 08.special-var.sh
+++ 08.special-var.sh
@@ -2,3 +2,8 @@
 
 echo "All variables passed this code: $@"
 echo "No of variables: $#"
+echo "script name: $0"
+echo "current directory: $pwd"
+echo "user running this script: $USER"
+echo "Home directory of the user: $HOME"
+echo "PID of the script: $$"
