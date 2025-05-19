3c40b1f70bbfe7382bef3a6cb2cd7dcf2aa17e03
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-19 11:12:22 +0530

    hello world started

--- 09.conditions.sh
+++ 09.conditions.sh
@@ -6,5 +6,5 @@ if [ $Number -lt 10 ]
 then
    echo "Given number $Number is less than 10"
 else
-   echo "Given number $Number is less than 10"
+   echo "Given number $Number is not less than 10"
 fi
