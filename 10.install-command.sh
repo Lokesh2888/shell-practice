8bd9cefd4391d4e5a5eeac4e7d609979c4a1f4ba
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-19 11:39:37 +0530

    hello world started

--- 10.install-command.sh
+++ 10.install-command.sh
@@ -5,7 +5,14 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
    echo "ERROR: Please run the script using root access"
-   exit 1
+   exit 1 #give other than 0 till 127 to exit the script
 fi
 
 dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+   echo "Installing MYSQL is.... SUCCESS"
+else
+   echo "Installing MYSQL is.... FAILURE
+fi
