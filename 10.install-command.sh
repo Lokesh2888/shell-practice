91078847e83848cfd9f03e9cfab2ce82dc415707
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-19 11:47:54 +0530

    hello world started

--- 10.install-command.sh
+++ 10.install-command.sh
@@ -6,6 +6,8 @@ if [ $USERID -ne 0 ]
 then
    echo "ERROR: Please run the script using root access"
    exit 1 #give other than 0 till 127 to exit the script
+else
+   echo "You are running with root access"
 fi
 
 dnf install mysqlwedds -y
