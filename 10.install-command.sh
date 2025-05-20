9eeaca23a86c0aaad6e1b6333e335ab632054311
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-20 10:43:00 +0530

    hello world started

--- 10.install-command.sh
+++ 10.install-command.sh
@@ -11,6 +11,9 @@ else
 fi
 
 dnf list installed mysql
+
+#check already installed or not. if installed $? is 0, then 
+#If not installed $? is not 0. expression is true.
 if [ $? -ne 0 ]
 then
    echo "MYSQL is not installed...and it is going to INSTALL"
---
+++ 11.functions.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+   echo "ERROR: Please run the script using root access"
+   exit 1 #give other than 0 till 127 to exit the script
+else
+   echo "You are running with root access"
+fi
+
+VALIDATE(){
+     if [ $1 -eq 0 ]
+   then
+      echo "Installing $2 is.... SUCCESS"
+   else
+      echo "Installing $2 is.... FAILURE"
+      exit 1
+    fi
+}
+
+dnf list installed mysql
+
+#check already installed or not. if installed $? is 0, then 
+#If not installed $? is not 0. expression is true.
+if [ $? -ne 0 ]
+then
+   echo "MYSQL is not installed...and it is going to INSTALL"
+   dnf install mysql -y
+   VALIDATE $? "MYSQL"
+else
+   echo "MYSQL is already INSTALLED...nothing to do"
+   exit 1   
+fi
+
+dnf list installed paython3
+
+#check already installed or not. if installed $? is 0, then 
+#If not installed $? is not 0. expression is true.
+if [ $? -ne 0 ]
+then
+   echo "python3 is not installed...and it is going to INSTALL"
+   dnf install python3 -y
+   VALIDATE $? "python3"
+else
+   echo "python3 is already INSTALLED...nothing to do"
+   exit 1   
+fi
+
+dnf list installed nginx
+
+#check already installed or not. if installed $? is 0, then 
+#If not installed $? is not 0. expression is true.
+if [ $? -ne 0 ]
+then
+   echo "nginx is not installed...and it is going to INSTALL"
+   dnf install nginx -y
+   VALIDATE $? "nginx"
+else
+   echo "nginx is already INSTALLED...nothing to do"
+   exit 1   
+fi
