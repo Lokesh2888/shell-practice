eb8b26ec63d4bc5f4371bb9949f4dee3e7b904c5
Lokesh <lokesh.kumar131991@gmail.com>
2025-05-28 13:16:37 +0530

    hello world started

---
+++ 05.variable-read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Enter you pin number"
+
+read -s PIN
+
+echo "Your pin number is $PIN"
---
+++ script-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Greeting="Hello world"
+echo "$Greeting"
+echo "PID of script 1 :$$"
+
+./script-2.sh
